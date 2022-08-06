bee201203772c1ad6cc27120dfb5cd0805f241d0
Mokarakom <mokarakom@anthro.ltd>
2022-08-06 07:09:18 +0700

    a

--- __index.php
+++ __index.php
@@ -1,1 +1,15 @@
-IT WORK!
+<?php
+
+//for local dev
+$baseUrl = "/at/";
+
+//request URI
+$uri = $_SERVER['REQUEST_URI'];
+$query = $_SERVER['QUERY_STRING'];
+
+if($uri != "" && $baseUrl != ""){
+    $uriDev = explode($baseUrl,$uri);
+    var_dump($uriDev);
+}
+
+var_dump($_SERVER);
